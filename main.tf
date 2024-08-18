9f5ea37f38aa109565913fedbb8731e8a1dea01f
jeremyjpj0916 <31913027+jeremyjpj0916@users.noreply.github.com>
2024-08-17 20:49:14 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,9 +3,9 @@ provider "aws" {
 }
 
 resource "aws_lambda_function" "metaai_dailytips_socials" {
-  filename      = "lambda_function_payload.zip"
+  filename      = "tips_generator.zip"
   function_name = "metaai-dailytips-socials"
-  handler       = "main.lambda_handler"
+  handler       = "tips_generator.py"
   runtime       = "python3.9"
   role          = aws_iam_role.lambda_execution_role.arn
 }
@@ -26,33 +26,6 @@ resource "aws_iam_role" "lambda_execution_role" {
       }
     ]
   })
-}
-
-resource "aws_api_gateway_rest_api" "metaai_dailytips_socials" {
-  name        = "metaai-dailytips-socials"
-  description = "API Gateway for Lambda function"
-}
-
-resource "aws_api_gateway_resource" "proxy" {
-  rest_api_id = aws_api_gateway_rest_api.metaai_dailytips_socials.id
-  parent_id   = aws_api_gateway_rest_api.metaai_dailytips_socials.root_resource_id
-  path_part   = "{proxy+}"
-}
-
-resource "aws_api_gateway_method" "proxy" {
-  rest_api_id = aws_api_gateway_rest_api.metaai_dailytips_socials.id
-  resource_id = aws_api_gateway_resource.proxy.id
-  http_method = "ANY"
-  authorization = "NONE"
-}
-
-resource "aws_api_gateway_integration" "lambda" {
-  rest_api_id = aws_api_gateway_rest_api.metaai_dailytips_socials.id
-  resource_id = aws_api_gateway_resource.proxy.id
-  http_method = aws_api_gateway_method.proxy.http_method
-  integration_http_method = "POST"
-  type        = "LAMBDA"
-  uri         = "arn:aws:apigateway:us-east-1:lambda:path/2015-03-31/functions/arn:aws:lambda:us-east-1:123456789012:function:metaai-dailytips-socials/invocations"
 }
 
 resource "aws_s3_bucket" "metaai_dailytips_socials" {
