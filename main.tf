96db96cfea4fa73d845c7159a6a3aa2234a0cc4a
jeremyjpj0916 <31913027+jeremyjpj0916@users.noreply.github.com>
2024-08-17 20:50:15 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,6 +8,11 @@ resource "aws_lambda_function" "metaai_dailytips_socials" {
   handler       = "tips_generator.py"
   runtime       = "python3.9"
   role          = aws_iam_role.lambda_execution_role.arn
+  environment {
+    variables = {
+      BUCKET_NAME = aws_s3_bucket.metaai_dailytips_socials.id
+    }
+  }
 }
 
 resource "aws_iam_role" "lambda_execution_role" {
@@ -28,7 +33,60 @@ resource "aws_iam_role" "lambda_execution_role" {
   })
 }
 
+resource "aws_iam_policy" "lambda_execution_policy" {
+  name        = "lambda-execution-policy"
+  description = "Policy for Lambda execution role"
+
+  policy      = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ]
+        Resource = "arn:aws:logs:*:*:*"
+        Effect    = "Allow"
+      },
+      {
+        Action = [
+          "s3:GetObject",
+          "s3:PutObject"
+        ]
+        Resource = aws_s3_bucket.metaai_dailytips_socials.arn
+        Effect    = "Allow"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_execution_policy_attachment" {
+  role       = aws_iam_role.lambda_execution_role.name
+  policy_arn = aws_iam_policy.lambda_execution_policy.arn
+}
+
 resource "aws_s3_bucket" "metaai_dailytips_socials" {
   bucket = "metaai-dailytips-socials"
   acl    = "private"
+}
+
+resource "aws_s3_bucket_policy" "metaai_dailytips_socials_policy" {
+  bucket = aws_s3_bucket.metaai_dailytips_socials.id
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "s3:GetObject",
+          "s3:PutObject"
+        ]
+        Resource = aws_s3_bucket.metaai_dailytips_socials.arn
+        Effect    = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      }
+    ]
+  })
 }
